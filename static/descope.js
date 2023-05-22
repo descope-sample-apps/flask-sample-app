b894a865ae0f6128aa51486bc2b20aee84d87335
Eric Sheen <eric.sprog@gmail.com>
2023-05-22 18:32:51 -0400

    removed refresh if statement from descope.js

--- static/descope.js
+++ static/descope.js
@@ -1,10 +1,7 @@
-const projectId = "P2Pq03wpnnjxJajSUKDuy2xviLNV"
+const projectId = ""
 const sdk = Descope({ projectId: projectId, persistTokens: true, autoRefresh: true })
 
 const sessionToken = sdk.getSessionToken()
 
 const refreshToken = sdk.getRefreshToken()
 const validRefreshToken = refreshToken && !sdk.isJwtExpired(refreshToken)
-if (validRefreshToken) {
-    sdk.refresh()
-}
