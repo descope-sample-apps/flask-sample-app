cbf99f0d84bac691248712346ccef38b0ac53437
Eric Sheen <eric.sprog@gmail.com>
2023-05-26 18:58:34 -0400

    protected get endpoint

--- app.py
+++ app.py
@@ -17,17 +17,20 @@ except Exception as error:
 def token_required(f): # auth decorator
     @wraps(f)
     def decorator(*args, **kwargs):
+        # print(request.headers['Authorization'])
         session_token = None
         if 'Authorization' in request.headers: # check if token in request
             auth_request = request.headers['Authorization']
             session_token = auth_request.replace('Bearer ', '')
         if not session_token: # throw error
-            return make_response(jsonify({"error": "❌ invalid session token!"}), 401)
+            return make_response(jsonify({"error": "❌ invalid session token in the request!"}), 401)
 
         try: # validate token
             jwt_response = descope_client.validate_session(session_token=session_token)
         except:
             return make_response(jsonify({"error": "❌ invalid session token!"}), 401)
+
+        print(jwt_response)
 
         return f(jwt_response, *args, **kwargs)
 
@@ -49,10 +52,10 @@ def profile():
     return render_template("profile.html")
 
 
-@app.route('/get_profile')
+@app.route('/get_profile', methods=["GET"])
 @token_required
 def get_profile(jwt_response):
-    return jwt_response
+    return {"jwt_response": jwt_response["jwt"]}
 
 
 if __name__ == "main":
--- static/descope.js
+++ static/descope.js
@@ -1,6 +1,5 @@
-const projectId = "P2QISti0x5gz0jXkMWwLKY3VZDcJ"
+const projectId = ""
 const sdk = Descope({ projectId: projectId, persistTokens: true, autoRefresh: true })
-
 const sessionToken = sdk.getSessionToken()
 
 // const refreshToken = sdk.getRefreshToken()
